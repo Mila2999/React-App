de32c2e7a508301843424814ddabd6bdc85aeea3
mila2999 <mila.savonik@gmail.com>
2023-08-23 11:27:43 +0300

    creating and using custom componenents

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import keyConceptsImage from './assets/images/key-concepts.png';
 import componentsImage from './assets/images/components.png';
 import stateImage from './assets/images/state.png';
 import eventsImage from './assets/images/events.png';
+import Header from './components/Header/Header';
+import Concept from './components/Concept/Concept';
 
 const concepts = [
   {
@@ -27,27 +28,29 @@ const concepts = [
 function App() {
   return (
     <div>
-      <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
+      <Header />
       <ul id="concepts">
-        <li className="concept">
-          <img src={concepts[0].image} alt={concepts[0].title} />
-          <h2>{concepts[0].title}</h2>
-          <p>{concepts[0].description}</p>
-        </li>
-        <li className="concept">
-          <img src={concepts[1].image} alt={concepts[1].title} />
-          <h2>{concepts[1].title}</h2>
-          <p>{concepts[1].description}</p>
-        </li>
+        <Concept
+          image={concepts[0].image}
+          title={concepts[0].title}
+          description={concepts[0].description}
+        />
+        <Concept
+          image={concepts[1].image}
+          title={concepts[1].title}
+          description={concepts[1].description}
+        />
+        <Concept
+          image={concepts[2].image}
+          title={concepts[2].title}
+          description={concepts[2].description}
+        />
+        {/* 
         <li className="concept">
           <img src={concepts[2].image} alt={concepts[2].title} />
           <h2>{concepts[2].title}</h2>
           <p>{concepts[2].description}</p>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
